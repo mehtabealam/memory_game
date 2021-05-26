e3390d12889ff6db1f5e430f80c1492a68f5b107
Jubair <jubair.a@juegostudios.com>
2021-05-26 23:33:28 +0530

    Update FacebookAudiance.js

--- memoryGame/assets/scripts/managers/FacebookAudiance.js
+++ memoryGame/assets/scripts/managers/FacebookAudiance.js
@@ -25,13 +25,13 @@ cc.Class({
     onLoad () {
         this.placementId = PLACEMENT_IDS.ANDROID;
 
-        cc.log("plat form is ",cc.sys.platform,cc.sys.IPAD);
+        cc.log("platform is ",cc.sys.platform,cc.sys.IPAD);
         if (cc.sys.platform === cc.sys.IPHONE || cc.sys.platform === cc.sys.IPAD) {
             this.placementId = PLACEMENT_IDS.IOS;
         }
     },
 
-    banner_click(event){
+    showBanner(event){
         let banner = new cc.Ads.Banner(this.placementId.BANNER,cc.Ads.BANNER_POSITION.ALIGN_PARENT_BOTTOM);
         this.banner = banner;
         banner.on("onAdLoaded", () => {
@@ -46,13 +46,6 @@ cc.Class({
             cc.log("banner onAdClicked");
         }, this.node);
 
-        // banner.loadAd().then(() => {
-        //     cc.log("banner show-------");
-        // }).catch((e) => {
-        //     this.status_tips.string = "banner reject";
-        //     cc.log("banner reject");
-        // });
-
         banner.show().then(() => {
             cc.log("banner show-------");
         }).catch((e) => {
@@ -60,11 +53,11 @@ cc.Class({
         });
     },
 
-    banner_destroy(){
+    destryBanner(){
         this.banner.destroy();
     },
 
-    interstital_click(){
+    showInterstital(){
         this.interstital = new cc.Ads.Interstitial(this.placementId.INTERSTITIAL);
         this.interstital.on("onInterstitialDisplayed", () => {
             cc.log("interstital onInterstitialDisplayed");
